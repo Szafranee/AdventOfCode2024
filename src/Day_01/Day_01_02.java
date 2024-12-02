d9dc7633cd533e43b30fe83b8fc9a858bfd575f5
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-12-02 18:08:00 +0100

    Modify part 2

--- src/Day_01/Day_01_02.java
+++ src/Day_01/Day_01_02.java
@@ -2,14 +2,13 @@ package Day_01;
 
 import java.io.BufferedReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.HashMap;
-import java.util.stream.Collectors;
 
 public class Day_01_02 {
     public static void main(String[] args) {
         ArrayList<Integer> locationsA = new ArrayList<>();
-        ArrayList<Integer> locationsB = new ArrayList<>();
+        HashMap<Integer, Integer> locationsB = new HashMap<>();
         int similarityScore = 0;
 
         try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader("src/Day_01/data_01.txt"))) {
@@ -17,23 +16,19 @@ public class Day_01_02 {
             while ((line = bufferedReader.readLine()) != null) {
                 String[] parts = line.split(" {3}");
                 locationsA.add(Integer.parseInt(parts[0]));
-                locationsB.add(Integer.parseInt(parts[1]));
+                locationsB.put(Integer.parseInt(parts[1]), locationsB.getOrDefault(Integer.parseInt(parts[1]), 0) + 1);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        HashMap<Integer, Integer> locationsCount = new HashMap<>();
-
-        Arrays.stream(locationsB.toArray()).collect(Collectors.groupingBy(l -> l)).forEach((k, v) -> locationsCount.put((Integer) k, v.size()));
-
         for (var loc : locationsA) {
-            Integer locCount = locationsCount.get(loc);
+            Integer locCount = locationsB.get(loc);
             if (locCount != null) {
                 similarityScore += locCount * loc;
             }
         }
 
-        System.out.println(similarityScore);
+        System.out.println("Similarity Score: " + similarityScore);
     }
 }
