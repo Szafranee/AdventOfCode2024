56f61c04fe1902bb6a33a0bf6895957bdc8cb39b
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-12-03 19:17:39 +0100

    Day 2 part 2

---
+++ src/Day_02/Day_02_02.java
@@ -0,0 +1,71 @@
+package Day_02;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Day_02_02 {
+    public static void main(String[] args) {
+        int safeReportCounter = 0;
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of("src/Day_02/data_02.txt"))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                ArrayList<Integer> levels = Arrays.stream(line.split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toCollection(ArrayList::new));
+
+                if (isSafe(levels)) {
+                    safeReportCounter++;
+                    continue;
+                }
+
+                if (canBeMadeSafe(levels)) {
+                    safeReportCounter++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Number of safe reports: " + safeReportCounter);
+    }
+
+    private static boolean isSafe(ArrayList<Integer> levels) {
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        for (int i = 0; i < levels.size() - 1; i++) {
+            int diff = levels.get(i) - levels.get(i + 1);
+
+            if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+                return false;
+            }
+
+            if (diff < 0) {
+                isDecreasing = false;
+            } else if (diff > 0) {
+                isIncreasing = false;
+            } else {
+                return false;
+            }
+        }
+
+        return isIncreasing || isDecreasing;
+    }
+
+    // we check every possibility
+    private static boolean canBeMadeSafe(ArrayList<Integer> levels) {
+        for (int i = 0; i < levels.size(); i++) {
+            ArrayList<Integer> newLevels = new ArrayList<>(levels);
+            newLevels.remove(i);
+            if (isSafe(newLevels)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
